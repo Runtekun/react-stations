b16b769bfe02ef27401b2817dac3feeb6dda9aec
dec457108 <yuuki1997s@i.softbank.jp>
2025-11-17 19:10:05 +0900

    サイトの説明と犬の画像を表示しよう

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,14 @@ export const App = () => {
     <header className="App-header">
       <h1>Dogアプリ</h1>
     </header>
+
+    <main>
+    <img
+      src="https://images.dog.ceo/breeds/papillon/n02086910_8590.jpg"
+      alt="パピヨンの画像"
+    />
+    <p>犬の画像を表示するサイトです。かわいい犬をどうぞ！</p>
+    </main>
     </div>
   )
 }
