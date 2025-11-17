aeb51b1810c7c76846c62d698936fcc89822b72a
dec457108 <yuuki1997s@i.softbank.jp>
2025-11-17 20:06:17 +0900

    ボタンのクリック時に stateを更新しよう

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ export const App = () => {
     "https://images.dog.ceo/breeds/papillon/n02086910_8590.jpg"
   );
 
+  const handleUpdate = () => {
+    console.log("画像を更新します"); 
+    setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg");
+  };
+
   return (
     <div>
     <header className="App-header">
@@ -21,9 +26,11 @@ export const App = () => {
     <main>
     <img
       src={dogUrl}
-      alt="パピヨンの画像"
+      alt="かわいい犬の画像"
     />
     <p>犬の画像を表示するサイトです。かわいい犬をどうぞ！</p>
+
+    <button onClick={handleUpdate}>更新</button>
     </main>
     </div>
   )
